9874b1b81516529104f65137d450240b61444e19
Regal9527 <102719950+Regal9527@users.noreply.github.com>
2023-03-13 23:10:21 +0530

    Update first-app.js

--- first-app.js
+++ first-app.js
@@ -1,7 +1,7 @@
-const http = require('http');
+ const http = require('http');
 
 const server = http.createServer((request, response) => {
-	response.end('Hello from different World\n');
+	response.end('Hello shekhar you have got selected  CONGRATULATIONS !!!! \n');
 });
 
 server.listen(7000, () => {
